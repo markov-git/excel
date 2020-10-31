166bf08e390754b4035be5bbddbdc96d510f10ae
markov-git <markovartyom795@gmail.com>
2020-10-31 16:19:10 +0300

    finish Emitter

--- src/core/dom.js
+++ src/core/dom.js
@@ -49,6 +49,10 @@ class Dom {
         return this.$el.getBoundingClientRect()
     }
 
+    find(selector) {
+        return $(this.$el.querySelector(selector))
+    }
+
     findAll(selector) {
         return this.$el.querySelectorAll(selector)
     }
@@ -57,6 +61,31 @@ class Dom {
         Object
             .keys(styles)
             .forEach(key => this.$el.style[key] = styles[key])
+    }
+
+    id(parse) {
+        if (parse) {
+            const parsed = this.id().split(':')
+
+            return {
+                row: +parsed[0],
+                col: +parsed[1],
+            }
+        }
+        return this.data.id
+    }
+
+    focus() {
+        this.$el.focus()
+        return this
+    }
+
+    addClass(className){
+         this.$el.classList.add(className)
+    }
+
+    removeClass(className){
+        this.$el.classList.remove(className)
     }
 }
 
