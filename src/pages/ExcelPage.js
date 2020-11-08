9f9b41369290112d1fbd7320d078f402b15d3543
markov-git <markovartyom795@gmail.com>
2020-11-08 14:06:37 +0300

    Implement DIP

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,32 +1,33 @@
-import {Page} from "@core/Page";
+import {Page} from "@core/page/Page";
 import {createStore} from "@core/store/createStore";
 import {rootReducer} from "@/redux/rootReducer";
-import {debounce, storage} from "@core/utils";
 import {Excel} from "@/components/excel/Excel";
 import {normalizeInitialState} from "@/redux/initialState";
 import {Header} from "@/components/header/Header";
 import {Toolbar} from "@/components/toolbar/Toolbar";
 import {Formula} from "@/components/formula/Formula";
 import {Table} from "@/components/table/Table";
+import {StateProcessor} from "@core/page/StateProcessor";
+import {LocalStorageClient} from "@/shared/LocalStorageClient";
 
-function storageName(param) {
-    return 'excel:' + param
-}
+
 
 export class ExcelPage extends Page {
-    getRoot() {
-        const params = this.params ? this.params : Date.now().toString()
+    constructor(param) {
+        super(param);
 
-        const state = storage(storageName(params))
+        this.storeSub = null
+        this.processor = new StateProcessor(
+            new LocalStorageClient(this.params)
+        )
+    }
 
+    async getRoot() {
+        const state = await this.processor.get()
         const initialState = normalizeInitialState(state)
         const store = createStore(rootReducer, initialState)
 
-        const stateListener = debounce(state => {
-            storage(storageName(params), state)
-        }, 300)
-
-        store.subscribe(stateListener)
+        this.storeSub = store.subscribe(this.processor.listen)
 
         this.excel = new Excel({
             components: [Header, Toolbar, Formula, Table],
@@ -42,5 +43,6 @@ export class ExcelPage extends Page {
 
     destroy() {
         this.excel.destroy()
+        this.storeSub.unsubscribe()
     }
 }
